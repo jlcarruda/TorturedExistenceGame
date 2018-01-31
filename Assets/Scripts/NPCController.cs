f8978dd27211c7b0ab0313598c34d781301c225c
Joao Lucas de Carvalho Arruda <jlcarruda3@gmail.com>
2018-01-31 19:08:41 -0200

    Initial Commit

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class NPCController : MonoBehaviour {
+
+    public Sprite deadSprite;
+
+    private bool is_dead = false;
+
+    public bool IsDead
+    {
+        get
+        {
+            return is_dead;
+        }
+
+        set
+        {
+            is_dead = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		if(name == "DeadBody")
+        {
+            is_dead = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (is_dead)
+        {
+            SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+            BoxCollider2D bc = gameObject.GetComponent<BoxCollider2D>();
+            sr.sprite = deadSprite; // Change to the Dead Sprite
+
+            Vector2 s = sr.sprite.bounds.size;
+            bc.size = s;
+            //bc.offset = new Vector2((s.x / 2), 0);
+            bc.isTrigger = true;
+
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask layerMask;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            teleportToBody();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            kill();
+        }
+	}
+
+    void teleportToBody()
+    {
+        // We will need to cast a ray from the camera to the point where the mouse was clicked
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+            NPCController npcController = hit.collider.gameObject.GetComponent<NPCController>();
+            if (npcController.IsDead)
+            {
+                var pos = transform.position;
+                transform.position = npcController.transform.position;
+                npcController.transform.position = pos;
+            }
+        }
+    }
+
+    void kill()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if(hit.collider != null)
+        {
+            NPCController npcController = hit.collider.gameObject.GetComponent<NPCController>();
+            npcController.IsDead = true;
+        }
+    }
+}
